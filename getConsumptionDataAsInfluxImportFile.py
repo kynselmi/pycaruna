f28aaf3ad16342b78c4b0b1490ba44a8467996d7
Aleksi Kärkkäinen <kynaaja@gmail.com>
2023-02-05 12:57:06 +0200

    Add options to get hourly, daily and monthly data from certain time interval until current time

--- getConsumptionDataAsInfluxImportFile.py
+++ getConsumptionDataAsInfluxImportFile.py
@@ -2,7 +2,7 @@ import getopt
 import os
 
 import os
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 import sys
 from pycaruna import CarunaPlus, TimeSpan
 import datetime as dt
@@ -57,44 +57,53 @@ def main(argv):
     metering_points = client.get_assets(customer_id)
     # print(metering_points)
 
-    # Get daily usage for the month of January 2023 for the first metering point. Yes, this means TimeSpan.MONTHLY. If
+    asset_id = metering_points[0]['assetId']
+    start_date = date.today() if not dateArg else datetime.strptime(dateArg, '%d-%m-%Y')
+    end_date = make_min_hour_datetime(date.today())
+     # Get daily usage for the month of January 2023 for the first metering point. Yes, this means TimeSpan.MONTHLY. If
     # you want hourly usage, use TimeSpan.DAILY.
+    delta = timedelta(days=1)
     match timespan:
         case 'hours':
             caruna_timespan = TimeSpan.DAILY
         case 'days':
             caruna_timespan = TimeSpan.MONTHLY
+            start_date = start_date.replace(day=1, hour=0, minute=0, second=0)
         case 'months':
             caruna_timespan = TimeSpan.YEARLY
         case _:
             raise Exception('Could not convert command line argument timestamp to Caruna timestamp. Timestamp: '+timespan)
-
-    asset_id = metering_points[0]['assetId']
-    # Fetch data from midnight 00:00 7 days ago to 23:59 today
-    readFromDate = date.today() if not dateArg else datetime.strptime(dateArg, '%d-%m-%Y')
-    consumption = client.get_energy(customer_id, asset_id, caruna_timespan, readFromDate.year, readFromDate.month, readFromDate.day)
-    filtered_consumption = [x for x in consumption if 'totalConsumption' in x]
-    # Extract the relevant data, filter out days without values (usually the most recent datapoint)
-    mapped_consumption = list(map(lambda item: {
-        'metering_point':  metering_points[0]['id'],
-        'timestamp': (int)(time.mktime(datetime.fromisoformat(item['timestamp']).timetuple())),
-        'kwh_total': item['totalConsumption'],
-        'timespan': timespan
-    }, filtered_consumption))
-
-    influx_input = map(
-        lambda item: "electricity_consumption,metering_company=Caruna,metering_point=%s,timespan=%s kwh_total=%s %s"
-            %(
-            item['metering_point'],
-            item['timespan'],
-            item['kwh_total'],
-            item['timestamp']
-            )
-        , mapped_consumption)
-
     print("# DML")
     print("# CONTEXT-DATABASE: electricity")
-    [print(i) for i in influx_input]
+
+    while start_date <= end_date:
+        consumption = client.get_energy(customer_id, asset_id, caruna_timespan, start_date.year, start_date.month, start_date.day)
+        if timespan == 'months':
+            start_date = start_date.replace(year=start_date.year+1)
+        elif timespan == 'days':
+            start_date = start_date.replace(month=start_date.month+1) 
+        else: 
+            start_date += delta
+        filtered_consumption = [x for x in consumption if 'totalConsumption' in x]
+        # Extract the relevant data, filter out days without values (usually the most recent datapoint)
+        mapped_consumption = list(map(lambda item: {
+            'metering_point':  metering_points[0]['id'],
+            'timestamp': (int)(time.mktime(datetime.fromisoformat(item['timestamp']).timetuple())),
+            'kwh_total': item['totalConsumption'],
+            'timespan': timespan
+        }, filtered_consumption))
+
+        influx_input = map(
+            lambda item: "electricity_consumption,metering_company=Caruna,metering_point=%s,timespan=%s kwh_total=%s %s"
+                %(
+                item['metering_point'],
+                item['timespan'],
+                item['kwh_total'],
+                item['timestamp']
+                )
+            , mapped_consumption)
+
+        [print(i) for i in influx_input]
 
 if __name__ == '__main__':
     main(sys.argv[1:])
